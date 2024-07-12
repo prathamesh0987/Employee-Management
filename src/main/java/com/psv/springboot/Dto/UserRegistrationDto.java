8671d1ace5ee42465bcbccbbbf3a6cd01c7c351c
Prathamesh <psvagare@gmail.com>
2024-07-12 12:44:47 +0530

    dto added

--- src/main/java/com/psv/springboot/Dto/UserRegistrationDto.java
+++ src/main/java/com/psv/springboot/Dto/UserRegistrationDto.java
@@ -1,5 +1,21 @@
 package com.psv.springboot.Dto;
 
 public class UserRegistrationDto {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    public UserRegistrationDto(){
+
+    }
+
+    public UserRegistrationDto(String firstName,String lastName,String email,String password){
+        super();
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.email=email;
+        this.password=password;
+    }
     
 }
