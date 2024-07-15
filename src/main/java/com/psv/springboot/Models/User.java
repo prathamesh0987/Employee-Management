01904ad1f3ba2989d804f25f06687493b56a3086
Prathamesh <psvagare@gmail.com>
2024-07-15 17:30:19 +0530

    issue resolved for login

--- src/main/java/com/psv/springboot/Models/User.java
+++ src/main/java/com/psv/springboot/Models/User.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "first_name")
@@ -30,9 +30,12 @@ public class User {
                     name = "user_id", referencedColumnName = "id"),
             inverseJoinColumns = @JoinColumn(
                     name = "role_id", referencedColumnName = "id"))
+
     private Collection<Role> roles;
 
-    // Getters and Setters
+    public User() {
+
+    }
 
     public User(String firstName, String lastName, String email, String password, Collection<Role> roles) {
         super();
@@ -42,52 +45,41 @@ public class User {
         this.password = password;
         this.roles = roles;
     }
-
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getFirstName() {
         return firstName;
     }
-
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-
     public String getLastName() {
         return lastName;
     }
-
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
-
     public Collection<Role> getRoles() {
         return roles;
     }
-
     public void setRoles(Collection<Role> roles) {
         this.roles = roles;
     }
+
 }
